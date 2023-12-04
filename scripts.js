bcfff4c1822440144ff98149802662777e4d9b73
Sergei <40200071+Sergei1790@users.noreply.github.com>
2023-12-04 10:30:38 +0200

    new

--- scripts.js
+++ scripts.js
@@ -23,7 +23,42 @@ const el1 = createPlayer('Elromco 1');
 const el2 = createPlayer('Elromco 2');
 console.log({el1});
 // console.log(gameboard.gameboard);
-function Cell(){
-    
+function cell(){
+    let value = 0;
+
+    // Accept a player's token to change the value of the cell
+    const addToken = (player) => {
+      value = player;
+    };
+  
+    // How we will retrieve the current value of this cell through closure
+    const getValue = () => value;
+  
+    return {
+      addToken,
+      getValue
+    };
 }
-el1
+const dropToken = (column, player) => {
+    // Our board's outermost array represents the row,
+    // so we need to loop through the rows, starting at row 0,
+    // find all the rows that don't have a token, then take the
+    // last one, which will represent the bottom-most empty cell
+    const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
+
+    // If no cells make it through the filter, 
+    // the move is invalid. Stop execution.
+    if (!availableCells.length) return;
+
+    // Otherwise, I have a valid cell, the last one in the filtered array
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addToken(player);
+  };
+
+  // This method will be used to print our board to the console.
+  // It is helpful to see what the board looks like after each turn as we play,
+  // but we won't need it after we build our UI
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  };
