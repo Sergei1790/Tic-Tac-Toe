197b57980811d2b42661dbb45e5a13eff998cce0
Sergei <40200071+Sergei1790@users.noreply.github.com>
2023-12-05 09:44:48 +0200

    new

--- scripts-visual.js
+++ scripts-visual.js
@@ -1,2 +1,32 @@
 const gameboard1 = document.querySelector('#sund-gameboard')
 
+
+const gameboard = (function () {
+    const board = [];
+    for(let row = 0; row<3; row++){
+        board[row]=[];
+        for(let col = 0; col<3; col++){
+            // board[row].push('Cell');
+            board[row].push(cell().getValue());
+        }
+    }
+    // console.table(board);
+    const getBoard = () => board;
+    return { getBoard };
+})();
+function cell() {
+    let value = 'Cell';
+  
+    // Accept a player's token to change the value of the cell
+    const addToken = (player) => {
+      value = player.token;
+    };
+  
+    // How we will retrieve the current value of this cell through closure
+    const getValue = () => value;
+  
+    return {
+      addToken,
+      getValue
+    };
+}
--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-function toggleClassOnClick(elem){
-    elem.classList.toggle('active');
-    elem.parentElement.classList.toggle('active')
-}
 
 // 1. Создать игровое поле 3 на 3
 // 2. Создать игрока 1
@@ -18,6 +14,7 @@ function toggleClassOnClick(elem){
 //         null,null,null,
 //         null,null,null]
 // }
+// function gameboard() {
 const gameboard = (function () {
     const board = [];
     let i = 1;
@@ -25,28 +22,30 @@ const gameboard = (function () {
      
         board[row]=[];
         for(let col = 0; col<3; col++){
-            board[row].push('Cell');
-            // board[row].push(Cell());
-
-            const boardCell = document.createElement('div');
-            boardCell.className ='board__cell';
-            boardCell.setAttribute('data-index', i);
-            boardCell.innerText = 'Cell';
-            gameboard1.appendChild(boardCell); 
+            // board[row].push('Cell');
+            board[row].push(Cell().getValue());
+            // const boardCell = document.createElement('div');
+            // boardCell.className ='board__cell';
+            // boardCell.setAttribute('data-index', i);
+            // boardCell.innerText = 'Cell';
+            // gameboard1.appendChild(boardCell); 
             i++;
         }
     }
     i = 0;
-    console.log(i);
-    // return {board};
-
-
-
-
-
+    // console.table(board);
     const getBoard = () => board;
+    // return { getBoard };
 
+
+
+    let chooseRow;
+    let chooseCell;
+    chooseRow = 0;
+    chooseCell = 1;
+    
     const dropToken = (column, player) => {
+
         // Our board's outermost array represents the row,
         // so we need to loop through the rows, starting at row 0,
         // find all the rows that don't have a token, then take the
@@ -58,76 +57,128 @@ const gameboard = (function () {
         if (!availableCells.length) return;
     
         // Otherwise, I have a valid cell, the last one in the filtered array
-        const lowestRow = availableCells.length - 1;
-        board[lowestRow][column].addToken(player);
+        board[chooseRow][chooseCell].addToken(player);
       };
         // This method will be used to print our board to the console.
         // It is helpful to see what the board looks like after each turn as we play,
         // but we won't need it after we build our UI
-        const printBoard = () => {
+        const printBoard = (playerToken) => {
             const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
             console.log(boardWithCellValues);
+            
+            // const newBoard = board.map(function (row, rowIndex) {
+            //     if(rowIndex === chooseRow){
+            //         return row.map(function (cell, cellIndex) {
+            //             return cellIndex === chooseCell ? playerToken : cell
+            //         });
+            //     } else{
+            //         return row;
+            //     }
+            // });
+            // console.log(newBoard);
         };
 
         // Here, we provide an interface for the rest of our
         // application to interact with the board
         return { getBoard, dropToken, printBoard };
 
-
+     
         
     
 })();
+// }
 
-
+function Cell() {
+    let value = 'Cell';
+  
+    // Accept a player's token to change the value of the cell
+    const addToken = (player) => {
+      value = player.token;
+    };
+  
+    // How we will retrieve the current value of this cell through closure
+    const getValue = () => value;
+  
+    return {
+      addToken,
+      getValue
+    };
+  }
 
 // console.table(gameboard.board);
-
-
-// let newBoard = gameboard.board;
-// newBoard[chooseRow][chooseCell] = 'X'
-// console.log(newBoard[chooseRow]);
-// newBoard.map(function () {
-//     return 'new Cell';
-// });
-
-
-// let newBoard = gameboard.board.map(function(cell) {
-//     return cell.length;
-// });
-// let newBoard = gameboard.board.map(item => item.length)
-
-// let newBoard = gameboard.board.map(function (row) {
-//     return row.map(function () {
-//         return 'new Cell';
-//     });
-// });
-
-
 
 function createPlayer (name,token) {
     // const token = name + '.token';
     return { name, token };
 }
 const el1 = createPlayer('Elromco 1', 'x');
-
 const el2 = createPlayer('Elromco 2', 'o');
+function GameController() {
+    const board = gameboard;
+    const players = [el1,el2];
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+
+    const printNewRound = () => {
+      board.printBoard();
+      console.log(`${getActivePlayer().name}'s turn.`);
+    };
+  
+    const playRound = (column) => {
+      // Drop a token for the current player
+      console.log(
+        `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+      );
+      board.dropToken(column, getActivePlayer().token);
+  
+      /*  This is where we would check for a winner and handle that logic,
+          such as a win message. */
+  
+      // Switch player turn
+      switchPlayerTurn();
+      printNewRound();
+    };
+  
+    // Initial play game message
+    printNewRound();
+  
+    // For the console version, we will only use playRound, but we will need
+    // getActivePlayer for the UI version, so I'm revealing it now
+    return {
+      playRound,
+      getActivePlayer
+    };
+  }
+  
+  const game = GameController();
+
 
 
 // console.log(gameboard.gameboard);
 
 
-let newBoard = gameboard.board;
-function updateBoard(playerToken){
-    newBoard = newBoard.map(function (row, rowIndex) {
-        if(rowIndex === chooseRow){
-            return row.map(function (cell, cellIndex) {
-                return cellIndex === chooseCell ? playerToken : cell
-            });
-        } else{
-            return row;
-        }
-    });
-}
+// let newBoard = gameboard.board;
+// let newBoard = gameboard.getBoard();
+// console.table(newBoard)
+// function updateBoard(playerToken){
+//     newBoard = newBoard.map(function (row, rowIndex) {
+//         if(rowIndex === chooseRow){
+//             return row.map(function (cell, cellIndex) {
+//                 return cellIndex === chooseCell ? playerToken : cell
+//             });
+//         } else{
+//             return row;
+//         }
+//     });
+// }
+
+
+
 // let chooseRow = 1;
 // let chooseCell = 2;
 
@@ -138,19 +189,19 @@ function updateBoard(playerToken){
 // updateBoard(el2.token)
 // console.table(newBoard);
 
-let chooseRow;
-let chooseCell;
+// let chooseRow;
+// let chooseCell;
 
-const gameRound = (function () {
-    // chooseRow = +prompt(`${el1.name} chooseRow`);
-    // chooseCell = +prompt(`${el1.name} chooseCell`);
-    chooseRow = 0;
-    chooseCell = 1;
-    updateBoard(el1.token);
-    // chooseRow = +prompt(`${el2.name} chooseRow`);
-    // chooseCell = +prompt(`${el2.name} chooseCell`);
-    chooseRow = 1;
-    chooseCell = 2;
-    updateBoard(el2.token);
-    console.table(newBoard);
-})()
+// const gameRound = (function () {
+//     // chooseRow = +prompt(`${el1.name} chooseRow`);
+//     // chooseCell = +prompt(`${el1.name} chooseCell`);
+//     chooseRow = 0;
+//     chooseCell = 1;
+//     updateBoard(el1.token);
+//     // chooseRow = +prompt(`${el2.name} chooseRow`);
+//     // chooseCell = +prompt(`${el2.name} chooseCell`);
+//     chooseRow = 1;
+//     chooseCell = 2;
+//     updateBoard(el2.token);
+//     console.table(newBoard);
+// })()
