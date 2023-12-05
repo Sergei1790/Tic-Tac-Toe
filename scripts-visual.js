08c34fc4f313403cc469048ffc927c7ed7584d5e
Sergei <40200071+Sergei1790@users.noreply.github.com>
2023-12-05 10:00:50 +0200

    new

--- scripts-visual.js
+++ scripts-visual.js
@@ -1,19 +1,18 @@
 const gameboard1 = document.querySelector('#sund-gameboard')
 
-
-const gameboard = (function () {
+function gameboard() {
     const board = [];
     for(let row = 0; row<3; row++){
         board[row]=[];
         for(let col = 0; col<3; col++){
-            // board[row].push('Cell');
             board[row].push(cell().getValue());
+            // board[row].push(cell().addToken(el1));
         }
     }
     // console.table(board);
     const getBoard = () => board;
     return { getBoard };
-})();
+}
 function cell() {
     let value = 'Cell';
   
@@ -21,7 +20,6 @@ function cell() {
     const addToken = (player) => {
       value = player.token;
     };
-  
     // How we will retrieve the current value of this cell through closure
     const getValue = () => value;
   
@@ -30,3 +28,12 @@ function cell() {
       getValue
     };
 }
+function createPlayer (name,token) {
+    return { name, token };
+}
+const el1 = createPlayer('Elromco 1', 'x');
+const el2 = createPlayer('Elromco 2', 'o');
+console.log(el1);
+
+console.table(gameboard().getBoard());
+console.log(cell().getValue());
